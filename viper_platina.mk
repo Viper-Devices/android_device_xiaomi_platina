c0dbe7a97fd424a4b36a6d42013c8762ecfbfbcd
Frank David Corona Prendes <frankdavid.corona@gmail.com>
2019-08-04 15:13:30 -0500

    Use of common full phone specification to prevent missing keyboard on initial boot.

--- viper_platina.mk
+++ viper_platina.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 $(call inherit-product, device/xiaomi/platina/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/viper/config/common.mk)
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
 PRODUCT_NAME := viper_platina
 VIPER_BUILD_TYPE := OFFICIAL
